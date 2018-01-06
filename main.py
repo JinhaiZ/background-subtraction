20d6880dea10c8d398a0d59a6d570778a36b8ea2
JinhaiZ <zhoujinhaixd@gmail.com>
2018-01-06 22:16:24 +0100

    add function binarization

--- main.py
+++ main.py
@@ -2,44 +2,25 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
-#Initiate
-cap = cv2.VideoCapture('test.avi')
-median_frames = 10
-index_P = 1.5
-index_Q = 120.4
-for i in xrange(400):
-    ret,frame = cap.read()
-images = np.zeros(shape=(60,80) + (median_frames,))
-for i in xrange(median_frames):
-    # read a frame, frame is (60, 80, 3) unit8 array
-    ret,frame = cap.read()
-    # resize frame, frame is (60, 80) float array
-    frame = np.median(frame,axis=2)
-    # add frame to images, images is (60, 80, median_frames) float array
-    images[:,:,i] = frame
+# define functions
 
-# get temporal median img, img is (60, 80) float array
-img = np.median(images, axis=2)
-# get the standard deviation value
-std = np.std(img)
-# get the average value
-avg = np.mean(img)
-# get binary image
-def f(pixel):
+def f(pixel, avg, std, index_P, index_Q):
+    # the Equation described in the paper
     if(pixel < avg - index_P*std and avg > index_Q) or (pixel > avg + index_P*std and avg <= index_Q):
         return 0
     else:
         return 1
+
 def binarization(image):
-    return  np.vectorize(f)(image)
-# binarization
-img = binarization(img)
-# component labeling
-# convert binary image to bw image and cast int to uint8
-img = np.uint8(img*255)
+    # get binary image
+    index_P = 1.5
+    index_Q = 120.4
+    # get the standard deviation value
+    std = np.std(image)
+    # get the average value
+    avg = np.mean(image)
+    return  np.vectorize(f)(image, avg, std, index_P, index_Q)
 
-
-# Connected Components Labeling
 def conComWithMorpOps(img):
     # Connected Components Labeling with morphological operations
 
@@ -74,6 +55,31 @@ def conComWithMorpOps(img):
     combined_img = np.uint8(combined_img)
     return combined_img
 
+#Initiate
+cap = cv2.VideoCapture('test.avi')
+median_frames = 10
+
+for i in xrange(400):
+    ret,frame = cap.read()
+images = np.zeros(shape=(60,80) + (median_frames,))
+for i in xrange(median_frames):
+    # read a frame, frame is (60, 80, 3) unit8 array
+    ret,frame = cap.read()
+    # resize frame, frame is (60, 80) float array
+    frame = np.median(frame,axis=2)
+    # add frame to images, images is (60, 80, median_frames) float array
+    images[:,:,i] = frame
+
+# get temporal median img, img is (60, 80) float array
+img = np.median(images, axis=2)
+
+# binarization
+img = binarization(img)
+
+# convert binary image to bw image and cast int to uint8
+img = np.uint8(img*255)
+
+# Connected Components Labeling
 combined_img = conComWithMorpOps(img)
 
 # create two subplots
